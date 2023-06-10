9e7850c890fca5e9a7a817c34acf4b406cd2ed92
Priscila Candido <priscila.candido@vixteam.com.br>
2023-06-10 13:58:52 -0300

    AJustes

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function exibirDialogo() {
+    var dialogo = document.getElementById('dialogo');
+    dialogo.style.display = 'block';
+}
+
+function fecharDialogo() {
+    var dialogo = document.getElementById('dialogo');
+    dialogo.style.display = 'none';
+}
